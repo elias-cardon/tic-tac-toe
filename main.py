4d641922569e91e71b753fbf79f7418eee84e9b2
Elias Cardon <62133614+elias-cardon@users.noreply.github.com>
2023-03-21 11:24:39 +0100

    On réarrange le code avec des constantes et des méthodes plus petites

--- main.py
+++ main.py
@@ -1,34 +1,46 @@
 import tkinter as tk
-from tkinter import messagebox, font
+from tkinter import font
+
+# Constantes
+BACKGROUND_COLOR = "white"
+BUTTON_COLOR = "light blue"
 
 class TicTacToe:
     def __init__(self):
-        # Créez la fenêtre tkinter et définissez son titre
+        self.init_window()
+        self.init_constants()
+        self.init_game()
+        self.create_buttons()
+        self.center_window()
+
+    def init_window(self):
+        """Initialise la fenêtre tkinter."""
         self.window = tk.Tk()
         self.window.title("Tic Tac Toe")
+        self.window.configure(bg=BACKGROUND_COLOR)
 
-        # Configurez la couleur de fond de la fenêtre
-        self.window.configure(bg="white")
+    def init_constants(self):
+        global BUTTON_FONT, WINNER_FONT, OK_BUTTON_FONT
+        BUTTON_FONT = font.Font(family="Helvetica", size=20, weight="bold")
+        WINNER_FONT = ("Helvetica", 16, "bold")
+        OK_BUTTON_FONT = ("Helvetica", 12, "bold")
 
-        # Initialisez le joueur actuel et le plateau de jeu
+    def init_game(self):
+        """Initialise les variables de jeu."""
         self.player = "X"
         self.board = [["" for _ in range(3)] for _ in range(3)]
+
+    def create_buttons(self):
+        """Crée les boutons du plateau de jeu et les ajoute à la fenêtre."""
         self.buttons = [[None for _ in range(3)] for _ in range(3)]
-
-        # Configurez la police des boutons
-        self.button_font = font.Font(family="Helvetica", size=20, weight="bold")
-
-        # Créez les boutons du plateau de jeu et ajoutez-les à la fenêtre
         for row in range(3):
             for col in range(3):
-                button = tk.Button(self.window, text="", width=10, height=3, bg="light blue", font=self.button_font, command=lambda r=row, c=col: self.play(r, c))
+                button = tk.Button(self.window, text="", width=10, height=3, bg=BUTTON_COLOR, font=BUTTON_FONT, command=lambda r=row, c=col: self.play(r, c))
                 button.grid(row=row, column=col, padx=5, pady=5)
                 self.buttons[row][col] = button
 
-        self.center_window()
-
-    # Centre la fenêtre sur l'écran
     def center_window(self):
+        """Centre la fenêtre sur l'écran."""
         self.window.update_idletasks()
         width = self.window.winfo_width()
         height = self.window.winfo_height()
@@ -82,21 +94,30 @@ class TicTacToe:
 class WinnerWindow(tk.Toplevel):
     def __init__(self, winner):
         super().__init__()
-        self.title("Résultat")
-        self.configure(bg="white")
-        self.geometry("300x100")
-
-        message = f"Le joueur {winner} a gagné !" if winner else "Match nul !"
-
-        label = tk.Label(self, text=message, font=("Helvetica", 16, "bold"), bg="white")
-        label.pack(pady=10)
-
-        ok_button = tk.Button(self, text="OK", font=("Helvetica", 12, "bold"), command=self.destroy)
-        ok_button.pack(pady=5)
-
+        self.init_window(winner)
+        self.create_message(winner)
+        self.create_ok_button()
         self.center_window()
 
+    def init_window(self, winner):
+        """Initialise la fenêtre des résultats."""
+        self.title("Résultat")
+        self.configure(bg=BACKGROUND_COLOR)
+        self.geometry("300x100")
+
+    def create_message(self, winner):
+        """Crée le message de résultat."""
+        message = f"Le joueur {winner} a gagné !" if winner else "Match nul !"
+        label = tk.Label(self, text=message, font=WINNER_FONT, bg=BACKGROUND_COLOR)
+        label.pack(pady=10)
+
+    def create_ok_button(self):
+        """Crée le bouton OK."""
+        ok_button = tk.Button(self, text="OK", font=OK_BUTTON_FONT, command=self.destroy)
+        ok_button.pack(pady=5)
+
     def center_window(self):
+        """Centre la fenêtre sur l'écran."""
         self.update_idletasks()
         width = self.winfo_width()
         height = self.winfo_height()
